e3b2bf2a5d4119bd5f4e198bf3b0024d9bda712d
J Wokaty <jwokaty@users.noreply.github.com>
2021-06-25 22:30:13 -0400

    Edit setuptools.setup

--- setup.py
+++ setup.py
@@ -2,21 +2,17 @@ import setuptools
 
 setuptools.setup(
     name="jupyter-rstudio-proxy",
-    version='1.0dev',
+    version="1.0dev",
     url="https://github.com/jwokaty/jupyter-rstudio-proxy",
     author="J Wokaty",
     description="jwokaty@users.noreply.github.com",
-    packages=setuptools.find_packages(),
-	keywords=['Jupyter', 'RStudio'],
-	classifiers=['Framework :: Jupyter'],
+    packages=["jupyter-rstudio-proxy"],
     install_requires=[
-        'jupyter-server-proxy'
+        "jupyter-server-proxy"
     ],
     entry_points={
-        'jupyter_serverproxy_servers': [
-            'rstudio = jupyter_rstudio_proxy:setup_rstudio',
+        "jupyter_serverproxy_servers": [
+            "rstudio = jupyter_rstudio_proxy:setup_rstudio",
         ]
-    },
-    package_data={
     },
 )
